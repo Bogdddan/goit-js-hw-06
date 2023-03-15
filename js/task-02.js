06da47963cc82bddebd7584f3d2929b1bc1bc809
Bogdddan <bogdankrajnik95@gmail.com>
2023-03-15 19:55:42 +0200

    uib

--- js/task-02.js
+++ js/task-02.js
@@ -8,21 +8,3 @@ const ingredients = [
 ];
 
 
-
-// const firstElemLi = document.createElement("li");
-// firstElemLi.textContent = 'Potatoes'
-// firstElemLi.classList.add('item')
-
-
-
-const ingredientsList = document.querySelector('#ingredients');
-
-
-const addIli = ingredients.map(ingredient => {
-  const item = document.createElement("li");
-  item.classList.add('item');
-  item.textContent = ingredient;
-  return item;
-});
-
-ingredientsList.append(...addIli);
